3809ed19e3fcb6875ccea52c2d3f838803a47c94
Jorge Asus <jorgehn1995@gmail.com>
2021-05-21 11:21:01 -0600

    Agregar archivos de proyecto.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.clases;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        clsConexion conexion;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void IniciarConexion(object sender, RoutedEventArgs e)
+        {
+            this.conexion = new clsConexion();
+            DataTable datos = this.conexion.MostrarTodos();
+            this.gridResultados.ItemsSource = datos.DefaultView;
+        }
+        private void nombre_TextChanged()
+        {
+
+        }
+
+        private void nombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.btnGuardar.Content = "Actualizar";
+            DataRowView row = this.gridResultados.SelectedItem as DataRowView;
+            if (row !=null)
+            {
+                this.txtCodigo.Text = row.Row.ItemArray[0].ToString();
+                this.txtNombre.Text = row.Row.ItemArray[1].ToString();
+                this.txtCosto.Text = row.Row.ItemArray[2].ToString();
+                this.txtPrecio.Text = row.Row.ItemArray[3].ToString();
+                this.txtExistencias.Text = row.Row.ItemArray[4].ToString();
+                this.btnEliminar.IsEnabled = true;
+            }
+        }
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            string parametro = this.txtBuscar.Text;
+            DataTable datos;
+            if (parametro == "")
+            {
+                datos = this.conexion.MostrarTodos();
+            }
+            else
+            {
+                datos = this.conexion.Buscar(parametro);
+            }
+            
+            this.gridResultados.ItemsSource = datos.DefaultView;
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            this.btnGuardar.Content = "Guardar";
+            this.txtCodigo.Text = "Nuevo";
+            this.txtNombre.Text = "";
+            this.txtCosto.Text = "";
+            this.txtPrecio.Text = "";
+            this.txtExistencias.Text = "";
+            this.btnEliminar.IsEnabled = false;
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.txtCodigo.Text == "Nuevo")
+            {
+                this.conexion.Insertar(this.txtNombre.Text, this.txtCosto.Text, this.txtPrecio.Text, this.txtExistencias.Text);
+            }
+            else
+            {
+                this.conexion.Actualizar(this.txtCodigo.Text,this.txtNombre.Text, this.txtCosto.Text, this.txtPrecio.Text, this.txtExistencias.Text);
+            }
+            DataTable datos = this.conexion.MostrarTodos();
+            this.gridResultados.ItemsSource = datos.DefaultView;
+            this.btnNuevo.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));///se hace click en el boton nuevo para limpar todos los campos
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.conexion.Eliminar(this.txtCodigo.Text);
+            DataTable datos = this.conexion.MostrarTodos();
+            this.gridResultados.ItemsSource = datos.DefaultView;
+            this.btnNuevo.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));///se hace click en el boton nuevo para limpar todos los campos
+        }
+    }
+}
---
+++ WpfApp2/clases/clsConexion.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp2.clases
+{
+    class clsConexion
+    {
+        private string servidor = "localhost"; //Nombre o ip del servidor de MySQL
+        private string bd = "tienda"; //Nombre de la base de datos
+        private string usuario = "root"; //Usuario de acceso a MySQL
+        private string password = ""; //Contraseña de usuario de acceso a MySQL
+        private string datos = null; //Variable para almacenar el resultado
+        private bool iniciado = false;
+        MySqlConnection conexionBD;
+        MySqlDataReader reader;
+        public clsConexion()
+        {
+            this.iniciar();
+        }
+        private void iniciar()
+        {
+            if (!iniciado)
+            {
+                //Crearemos la cadena de conexión concatenando las variables
+                string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User Id=" + usuario + "; Password=" + password + "";
+                //Instancia para conexión a MySQL, recibe la cadena de conexión
+                conexionBD = new MySqlConnection(cadenaConexion);
+                this.iniciado = true;
+            }
+        }
+        private DataTable ConsultaSimple(string consulta, int columna = 0)
+        {
+
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(consulta); //Declaración SQL para ejecutar contra una base de datos MySQL
+                comando.Connection = conexionBD; //Establece la MySqlConnection utilizada por esta instancia de MySqlCommand
+                conexionBD.Open(); //Abre la conexión
+
+                DataTable dt = new DataTable();
+                dt.Load(comando.ExecuteReader());
+                return dt;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                DataTable dt = new DataTable();
+                return dt;
+            }
+            finally
+            {
+                conexionBD.Close(); //Cierra la conexión a MySQL
+            }
+        }
+
+        public DataTable MostrarTodos()
+        {
+            this.iniciar();
+            string consulta = "select * from productos";
+            return this.ConsultaSimple(consulta);
+
+        }
+        public DataTable Buscar(string parametro)
+        {
+            this.iniciar();
+            string consulta = $"select * from productos where nombre like '%{parametro}%' or costo='{parametro}' or precio='{parametro}'  or existencias='{parametro}' or ganancia_unitaria='{parametro}' or ganancia_total='{parametro}'";
+            return this.ConsultaSimple(consulta);
+
+        }
+        public DataTable Insertar(string nombre, string costo, string precio, string existencia)
+        {
+            decimal ganancia_unitaria = Convert.ToDecimal(precio) - Convert.ToDecimal(costo);
+            decimal ganancia_total = ganancia_unitaria * Convert.ToDecimal(existencia);
+            string consulta = $"insert into productos (id, nombre, costo, precio, existencias, ganancia_unitaria, ganancia_total) VALUES ('0','{nombre}', '{costo}', '{precio}', '{existencia}', '{ganancia_unitaria}','{ganancia_total}');";
+            this.ConsultaSimple(consulta);
+            return this.MostrarTodos();
+        }
+
+        public DataTable Actualizar (string id,string nombre, string costo, string precio, string existencia)
+        {
+            decimal ganancia_unitaria = Convert.ToDecimal(precio) - Convert.ToDecimal(costo);
+            decimal ganancia_total = ganancia_unitaria * Convert.ToDecimal(existencia);
+            string consulta = $"Update productos set nombre='{nombre}',  costo='{costo}', precio='{precio}', existencias='{existencia}', ganancia_unitaria='{ganancia_unitaria}', ganancia_total='{ganancia_total}' where id={id}";
+            this.ConsultaSimple(consulta);
+            return this.MostrarTodos();
+        }
+
+        public DataTable Eliminar(string id)
+        {
+          
+            string consulta = $"Delete from productos  where id={id}";
+            this.ConsultaSimple(consulta);
+            return this.MostrarTodos();
+        }
+
+
+
+
+    }
+}
